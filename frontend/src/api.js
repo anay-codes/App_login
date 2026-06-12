4acac79037da3b17a3cfcc94bd7870d07ae72ae2
anay-codes <tareanay183@gmail.com>
2026-06-12 18:06:56 +0530

    Fix production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "/api",
+  baseURL: "https://app-login-po50.onrender.com/api",
   timeout: 15000
 });
 
